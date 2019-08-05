e6109599d902e35dfac2f6df42942cf492ea2170
yixuanh <yixuanhuang.gz@gmail.com>
2019-08-04 22:48:10 -0400

    initial commit

---
+++ controls.py
@@ -0,0 +1,110 @@
+STATES = dict(
+    AL = 'Alabama',
+    AK = 'Alaska',
+    AZ = 'Arizona',
+    AR = 'Arkansas',
+    CA = 'California',
+    CO = 'Colorado',
+    CT = 'Connecticut',
+    DE = 'Delaware',
+    DC = 'Discrict of Columbia',
+    FL = 'Florida',
+    GA = 'Geogia',
+    HI = 'Hawaii',
+    ID = 'Idaho',
+    IL = 'Illinois',
+    IN = 'Indiana',
+    IA = 'Iowa',
+    KS = 'Kansas',
+    KY = 'Kentucky',
+    LA = 'Louisiana',
+    ME = 'Maine',
+    MD = 'Maryland',
+    MA = 'Massachusetts',
+    MI = 'Michigan',
+    MN = 'Minnesota',
+    MS = 'Mississippi',
+    MO = 'Missouri',
+    MT = 'Montana',
+    NE = 'Nebraska',
+    NV = 'Nevada',
+    NH = 'New Hampshire',
+    NJ = 'New Jersey',
+    NM = 'New Mexico',
+    NY = 'New York',
+    NC = 'North Carolina',
+    ND = 'North Dakota',
+    OH = 'Ohio',
+    OK = 'Oklahoma',
+    OR = 'Oregon',
+    PA = 'Pennsylvania',
+    RI = 'Rhode Island',
+    SC = 'South Carolina',
+    SD = 'South Dakota',
+    TN = 'Tennessee',
+    TX = 'Texas',
+    UT = 'Utah',
+    VT = 'Vermont',
+    VA = 'Virginia',
+    WA = 'Washingotn',
+    WV = 'West Virginia',
+    WI = 'Wisconsin',
+    WY = 'Wyoming'
+    )
+    
+
+HEALTH_MEASURES = dict(
+    ARTHRITIS = 'Arthritis',
+    BPHIGH = 'High Blood Pressure',
+    CANCER = 'Cancer (excluding Skin Cancer)',
+    CASTHMA = 'Asthma',
+    CHD = 'Coronary Heart Disease',
+    COPD = 'Chronic Obstructive Pulmonary Disease',
+    COLON_SCREEN = 'Fecal Occult Blood Test',
+    DENTAL = 'Visits to Dentist',
+    MAMMOUSE = 'Mammography Use',
+    PAPTEST = 'Papanicolaou Smear Use',
+    SLEEP = 'Sleeping Less Than 7 Hours',
+    TEETHLOST = 'All Teeth Lost',
+    DIABETES = 'Diagnosed Diabetes',
+    HIGHCHOL = 'High Cholesterol',
+    KIDNEY = 'Chronic Kidney Diseas',
+    MHLTH = 'Mental Health Not Good for >=14 Days',
+    PHLTH = 'Physical Health Not Good for >=14 Days',
+    STROKE = 'Stroke',
+    ACCESS2 = 'No Health Insurance',
+    BPMED = 'Taking Medicine for High Blood Pressure',
+    CHECKUP = 'Routine Medical Checkup',
+    CHOLSCREEN = 'Cholesterol Screening',
+    BINGE = 'Binge Drinking',
+    CSMOKING = 'Smoking',
+    LPA = 'No Leisure-Time Physical Activity',
+    OBESITY = 'Obesity',
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
